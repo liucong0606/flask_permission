66b961cdab5433c816671c098fd7263023d97ed2
liucong0606 <1670847114@qq.com>
2020-07-11 10:55:24 +0800

    新增接口文档，和添加测试数据的sql文件

---
+++ doc/添加测试数据.sql
@@ -0,0 +1,6 @@
+-- 用户表 	// 密码（MD5加密）
+insert into
+	tab_user (username, password, gender, birthday, telephone, remark)
+values
+	('root', '63a9f0ea7bb98050796b649e85481845', '男', '1998-08-04', '18574394525', null),
+	('lisa', '202cb962ac59075b964b07152d234b70', '女', '1995-12-15', '18375497258', null);
