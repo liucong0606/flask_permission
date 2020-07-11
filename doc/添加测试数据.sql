69d13f5fde523a45338a90ae7e7b8e271b374a0e
liucong0606 <1670847114@qq.com>
2020-07-11 14:11:53 +0800

    备注了用户加密的密码

--- doc/添加测试数据.sql
+++ doc/添加测试数据.sql
@@ -2,5 +2,5 @@
 insert into
 	tab_user (username, password, gender, birthday, telephone, remark)
 values
-	('root', '63a9f0ea7bb98050796b649e85481845', '男', '1998-08-04', '18574394525', null),
-	('lisa', '202cb962ac59075b964b07152d234b70', '女', '1995-12-15', '18375497258', null);
+	('root', '63a9f0ea7bb98050796b649e85481845', '男', '1998-08-04', '18574394525', null),	-- 密码 root
+	('lisa', '202cb962ac59075b964b07152d234b70', '女', '1995-12-15', '18375497258', null);	-- 密码 123
